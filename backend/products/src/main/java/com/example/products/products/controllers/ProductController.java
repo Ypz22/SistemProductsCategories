0814392e27ef782e339a7c7ff44f50be93523478
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-07-31 16:58:56 -0500

    First commit

---
+++ backend/products/src/main/java/com/example/products/products/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package com.example.products.products.controllers;
+
+import com.example.products.products.models.entities.Product;
+import com.example.products.products.service.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private  final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable long id) {
+        return productService.getProductById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createProduct(@Valid @RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(product));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@Valid @PathVariable long id, @RequestBody Product product) {
+        try{
+            return ResponseEntity.ok(productService.updateProduct(id, product));
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
