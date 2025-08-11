8abb55ff7cd2c875b250e37a4b8ffcee59953c75
Jefferson Yepez <jeffersonyepez621@gmail.com>
2025-08-10 22:18:56 -0500

    upload files

--- backend/products/src/main/java/com/example/products/products/controllers/ProductController.java
+++ backend/products/src/main/java/com/example/products/products/controllers/ProductController.java
@@ -1,15 +1,25 @@
 package com.example.products.products.controllers;
 
-import com.example.products.products.models.entities.Product;
-import com.example.products.products.service.ProductService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.products.products.models.entities.Product;
+import com.example.products.products.service.ProductService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 @RestController
 
 @RequestMapping("/api/products")
